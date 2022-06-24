3d13dd4810046e1d80c762f4de002c42e14af6b5
Carlos <Cbeckhauser@gmail.com>
2022-06-24 14:28:32 -0300

    exercicios de logica

--- calculations.js
+++ calculations.js
@@ -1,4 +1,27 @@
-import calc from "./calculator.js";
+import compare from "./calculator.js";
 
+const numberList = [
+  [5, 4],
+  [3, 4],
+  [0, 0],
+  [25, 30],
+  [157, 157.1],
+];
 
-calc(5, 3);
+/* for (let i = 0; i < numberList.length; i++) {
+  compare(numberList[i][0], numberList[i][1]);
+} */
+
+/* let i = 0;
+while (i < numberList.length) {
+  compare(numberList[i][0], numberList[i][1]);
+  i++;
+} */
+
+/* const result = compare(5, 4);
+console.log(result);
+ */
+
+numberList.map(([x, y]) => {
+  return compare(x, y);
+});
